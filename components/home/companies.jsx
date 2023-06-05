39be1911f64663e8a5bc9b1494f1f09f4ab7aa9e
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2023-06-05 22:11:02 +0100

    Done with some portions of page

--- components/home/companies.jsx
+++ components/home/companies.jsx
@@ -5,18 +5,20 @@ import applepay from '../../public/Apple_Pay_logo.svg'
 import Image from 'next/image'
 export default function Companies() {
     return (
-        <div className="mx-auto max-w-screen-lg flex  flex-row items-center justify-around text-gray-700 gap-2">
-            <div className="">
-                <Image src={paystack} width={200} height={200} alt='' />
-            </div>
-            <div className="">
-                <Image src={stripe} width={200} height={200} alt='' />
-            </div>
-            <div className="">
-                <Image src={gpay} width={200} height={200} alt='' />
-            </div>
-            <div className="">
-                <Image src={applepay} width={200} height={200} alt='' />
+        <div className='bg-orange-400'>
+            <div className="mx-auto max-w-screen-lg grid sm:flex items-center justify-around text-gray-700 gap-2">
+                <div className="">
+                    <Image src={paystack} width={120} height={120} alt='' />
+                </div>
+                <div className="">
+                    <Image src={stripe} width={120} height={120} alt='' />
+                </div>
+                <div className="">
+                    <Image src={gpay} width={120} height={120} alt='' />
+                </div>
+                <div className="">
+                    <Image src={applepay} width={120} height={120} alt='' />
+                </div>
             </div>
         </div>
     )
--- components/home/services.jsx
+++ components/home/services.jsx
@@ -1,23 +1,46 @@
 import Image from "next/image"
+import house from '../../public/house.svg'
+import office from '../../public/office.svg'
+import shield from '../../public/shield.svg'
+
+const services = [
+    {
+        'name': 'Home',
+        'img': shield,
+        'desc': 'Valuable protection 24/7',
+        'url': ''
+    },
+    {
+        'name': 'Home',
+        'img': office,
+        'desc': 'Protection for your business transactions',
+        'url': ''
+    },
+    {
+        'name': 'Home',
+        'img': house,
+        'desc': 'Personalised protection and detection of fraudulent activities',
+        'url': ''
+    }
+]
 export default function OurServices() {
     return (
-        <div>
-            <div className="text-center">
-                <h2 className="">Our Special <span>Services</span>for you</h2>
-                <p>Package euro flying. Package diary city trip booking budget frequent flyer outdoor
-                    tour operator New York City China wellness Asia
-                </p>
-            </div>
-            <div>
-                <div>
-                    image
-                    <h3>Corporate Infrastructure</h3>
+        <div className="">
+            <div className="mx-auto max-w-screen-lg items-center flex flex-col">
+                <div className="text-center text-gray-950 py-12 max-w-md">
+                    <h2 className="text-2xl font-bold">Our Special <span className="text-orange-400">Services</span> for you</h2>
+                    <p className="text-center">Package euro flying. Package diary city trip booking budget frequent flyer outdoor
+                        tour operator New York City China wellness Asia
+                    </p>
                 </div>
-                <div>
-                    <h3>Home Services</h3>
-                </div>
-                <div>
-                    <h3>Wirelss Network</h3>
+                <div className="flex flex-row ">
+                    {
+                        services.map((item) => <a key={item.name} href={item.url} className="flex flex-col mx-auto text-gray-950" >
+                            <Image src={item.img} height={0} width={0} alt={item.name} sizes='20vw' style={{ width: '35%', height: 'auto' }} className="mx-auto" />
+                            <p className="text-center py-2">{item.desc}</p>
+                        </a>
+                        )
+                    }
                 </div>
             </div>
         </div>
